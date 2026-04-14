74eb42a57f31f68783d4934d2371403a8c1d143a
haris49 <hariis.459@gmail.com>
2026-04-14 10:34:33 +0700

    menambahkan input elemen array menggunakan perulangan

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -26,3 +26,7 @@ void input()
     cout << "=============================================\n";
     for (int i = 0; i < nPanjang; i++)
     {
+        cout << "Data ke-" << (i + 1) << " = ";
+        cin >> element[i]; 
+    }
+}
